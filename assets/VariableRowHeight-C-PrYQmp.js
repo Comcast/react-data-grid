01cc1ad2406160dc7023117a5c9525108a6bd1f8
GitHub Action <action@github.com>
2026-02-18 16:00:58 +0000

    gh-pages deployment

--- assets/VariableRowHeight-C-PrYQmp.js
+++
@@ -1,2 +0,0 @@
-import{c as e,l as t,r as n}from"./index-DFJCUs9H.js";import{t as r}from"./DataGrid-cB-ENLtp.js";var i=t(),a=Array.from({length:500},(e,t)=>t),o=[];for(let e=0;e<30;e++){let t=String(e);o.push({key:t,name:t,frozen:e<5,resizable:!0,renderCell:n})}function s(){let t=e();return(0,i.jsx)(r,{"aria-label":`Variable Row Height Example`,columns:o,rows:a,rowHeight:c,className:`fill-grid`,direction:t})}function c(){return 25+Math.round(Math.random()*75)}export{s as component};
-//# sourceMappingURL=VariableRowHeight-C-PrYQmp.js.map
