944babe719989961c581de7d7ea2205bb546c39c
Nicolas Stepien <stepien.nicolas@gmail.com>
2026-02-23 22:16:53 +0000

    lint

--- src/utils/eventUtils.ts
+++ src/utils/eventUtils.ts
@@ -5,7 +5,6 @@ export function createCellEvent<E extends React.SyntheticEvent<HTMLDivElement>>(
 ): CellEvent<E> {
   let defaultPrevented = false;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return Object.create(event, {
     preventGridDefault: {
       value() {
