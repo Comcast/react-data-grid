9dba443b2c6f086d999d25ec98dfec36e9f67d51
Nicolas Stepien <stepien.nicolas@gmail.com>
2026-02-23 18:23:21 +0000

    fix

--- website/routes/CellNavigation.tsx
+++ website/routes/CellNavigation.tsx
@@ -85,7 +85,7 @@ function CellNavigation() {
   function handleCellKeyDown(args: CellKeyDownArgs<Row>, event: CellKeyboardEvent) {
     if (args.mode === 'EDIT') return;
     const { column, rowIdx, selectCell } = args;
-    const { idx } = column;
+    const idx = column?.idx ?? -1;
     const { key, shiftKey } = event;
 
     const preventDefault = () => {
