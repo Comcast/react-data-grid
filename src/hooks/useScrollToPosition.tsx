140c71480e0fc2fff3364e0e5da44119edc9255a
Nicolas Stepien <stepien.nicolas@gmail.com>
2026-02-27 14:45:55 +0000

    no hook, so we can inline the element

--- src/hooks/useScrollToPosition.tsx
+++ src/hooks/useScrollToPosition.tsx
@@ -17,43 +17,24 @@ export function useScrollToPosition({ gridRef }: Props) {
   return {
     setScrollToPosition,
     scrollToPositionElement: scrollToPosition && (
-      <ScrollToCell
-        gridRef={gridRef}
-        scrollToPosition={scrollToPosition}
-        setScrollToCellPosition={setScrollToPosition}
+      <div
+        ref={(div) => {
+          if (div === null) return;
+          const grid = gridRef.current!;
+          const { scrollLeft, scrollTop } = grid;
+          // scroll until the cell/column is completely visible
+          // this is needed if the grid has auto-sized columns
+          // setting the behavior to auto so it can be overridden
+          scrollIntoView(div, 'auto');
+          if (grid.scrollLeft === scrollLeft && grid.scrollTop === scrollTop) {
+            setScrollToPosition(null);
+          }
+        }}
+        style={{
+          gridColumn: scrollToPosition.idx === undefined ? '1/-1' : scrollToPosition.idx + 1,
+          gridRow: scrollToPosition.rowIdx === undefined ? '1/-1' : scrollToPosition.rowIdx + 1
+        }}
       />
     )
   } as const;
-}
-
-interface ScrollToCellProps extends Props {
-  scrollToPosition: PartialPosition;
-  setScrollToCellPosition: (cell: null) => void;
-}
-
-function ScrollToCell({
-  gridRef,
-  scrollToPosition: { idx, rowIdx },
-  setScrollToCellPosition
-}: ScrollToCellProps) {
-  return (
-    <div
-      ref={(div) => {
-        if (div === null) return;
-        const grid = gridRef.current!;
-        const { scrollLeft, scrollTop } = grid;
-        // scroll until the cell is completely visible
-        // this is needed if the grid has auto-sized columns
-        // setting the behavior to auto so it can be overridden
-        scrollIntoView(div, 'auto');
-        if (grid.scrollLeft === scrollLeft && grid.scrollTop === scrollTop) {
-          setScrollToCellPosition(null);
-        }
-      }}
-      style={{
-        gridColumn: idx === undefined ? '1/-1' : idx + 1,
-        gridRow: rowIdx === undefined ? '1/-1' : rowIdx + 1
-      }}
-    />
-  );
 }
