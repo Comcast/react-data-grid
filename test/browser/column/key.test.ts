f8f6f96e07af790f051288cc41fd11869267a655
amahajan <amahajan@freewheel.tv>
2026-03-13 13:28:25 -0500

    - extra line

--- test/browser/column/key.test.ts
+++ test/browser/column/key.test.ts
@@ -9,7 +9,6 @@ test('key is escaped in query selectors', async () => {
     }
   ];
 
-  //
   // eslint-disable-next-line vitest/no-alias-methods
   await expect(setup({ columns, rows: [] })).resolves.not.toThrow();
 });
