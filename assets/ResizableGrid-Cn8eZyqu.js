549969856f458c77a948b75eb8a833c22d2e6061
GitHub Action <action@github.com>
2026-04-23 16:09:39 +0000

    gh-pages deployment

---
+++ assets/ResizableGrid-Cn8eZyqu.js
@@ -0,0 +1,2 @@
+import{a as e,i as t,r as n}from"./index-C_gWC6Qh.js";import{t as r}from"./DataGrid-Bq1XXGGN.js";var i=e(),a=Array.from({length:100},(e,t)=>t),o=[];for(let e=0;e<50;e++){let t=String(e);o.push({key:t,name:t,renderCell:n})}function s(){return(0,i.jsx)(r,{"aria-label":`Resizable Grid Example`,columns:o,rows:a,className:`fill-grid`,style:{resize:`both`},direction:t()})}export{s as component};
+//# sourceMappingURL=ResizableGrid-Cn8eZyqu.js.map
