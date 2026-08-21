0e720fc5f2ef56a95057f3577aa7ca217ea4b664
GitHub Action <action@github.com>
2026-08-21 16:58:01 +0000

    gh-pages deployment

--- assets/ResizableGrid-DStNnFNS.js
+++
@@ -1,2 +0,0 @@
-import{n as e,r as t}from"./directionContext-Cxp0yfQU.js";import{t as n}from"./renderCoordinates-ScwPfZqo.js";import{t as r}from"./src-DdL-63jY.js";var i=t(),a=Array.from({length:100},(e,t)=>t),o=[];for(let e=0;e<50;e++){let t=String(e);o.push({key:t,name:t,renderCell:n})}function s(){let t=e();return(0,i.jsx)(r,{"aria-label":`Resizable Grid Example`,columns:o,rows:a,className:`fill-grid`,style:{resize:`both`},direction:t})}export{s as component};
-//# sourceMappingURL=ResizableGrid-DStNnFNS.js.map
