ff12f0d391492ee88bb6079ca0354d4b59fa4c03
Nicolas Stepien <stepien.nicolas@gmail.com>
2026-08-29 13:10:39 +0100

    Move .cache, tweak eslint

--- vite.config.ts
+++ vite.config.ts
@@ -60,7 +60,7 @@ const playwrightOptions: PlaywrightProviderOptions = {
 
 export default defineConfig(({ isPreview }): ViteUserConfig => ({
   base: '/react-data-grid/',
-  cacheDir: '.cache/vite',
+  cacheDir: 'node_modules/.cache/vite',
   clearScreen: false,
   build: {
     // chunkImportMap: true,
